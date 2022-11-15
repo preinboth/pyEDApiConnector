511e76b5c48d4230beff3b9f09b41a204821e151
Peter Reinboth <github@preinboth.de>
2022-11-15 04:52:33 +0100

    #EDAC-33 API System - Get information about stations in a system / Tests
    - Test for querying the stations via system name and Id implemented

---
+++ tests/edsm/test_systemApiStation.py
@@ -0,0 +1,23 @@
+import unittest
+
+from src.connector.edsm.systemApi import station
+
+
+class TestSytemStation(unittest.TestCase):
+    def test_get_station(self):
+        json = station.getStation("Gacrux")
+        # System
+        assert json['id'] == 7564
+        assert json['id64'] == 672028239289
+        assert json['name'] == 'Gacrux'
+        # TODO: Stations checken
+
+    def test_get_station_by_id(self):
+        json = station.getStationById(9324)
+        assert json['id'] == 9324
+        assert json['id64'] == 2871051494841
+        assert json['name'] == 'Honoto'
+
+
+if __name__ == '__main__':
+    unittest.main()
