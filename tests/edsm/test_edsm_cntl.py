6aa298de094ced9229f2eabd0b95589531e1a80b
Peter Reinboth <github@preinboth.de>
2022-11-16 09:17:45 +0100

    #EDAC-44 API Systems - Get information about systems / Tests Test EDSM Controller get_Systems

--- tests/edsm/test_edsm_cntl.py
+++ tests/edsm/test_edsm_cntl.py
@@ -100,6 +100,33 @@ class EdsmApiTests(unittest.TestCase):
         self.assertEqual(27, json['id'])
         self.assertEqual("Sol", json['name'])
 
+    def test_getSystems(self):
+        json = edsmCntl.getSystems("Gacrux", "Achali")
+
+        # System Achali
+        system = json[0]
+        self.assertEqual("Achali", system['name'])
+        self.assertEqual(4532, system['id'])
+        self.assertEqual(3657332462314, system['id64'])
+        self.assertFalse(system['requirePermit'])
+        self.assertTrue(system['information'])
+
+        self.assertEqual("K (Yellow-Orange) Star", system['primaryStar']['type'])
+        self.assertEqual("Achali", system['primaryStar']['name'])
+        self.assertTrue(system['primaryStar']['isScoopable'])
+
+        # System Gacrux
+        system = json[1]
+        self.assertEqual("Gacrux", system['name'])
+        self.assertEqual(7564, system['id'])
+        self.assertEqual(672028239289, system['id64'])
+        self.assertFalse(system['requirePermit'])
+        self.assertTrue(system['information'])
+
+        self.assertEqual("M (Red giant) Star", system['primaryStar']['type'])
+        self.assertEqual("Gacrux", system['primaryStar']['name'])
+        self.assertTrue(system['primaryStar']['isScoopable'])
+
 
 if __name__ == '__main__':
     unittest.main()
