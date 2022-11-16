eb2775451d57b81657648a4cd3c3081753cfee9a
Peter Reinboth <github@preinboth.de>
2022-11-16 01:24:30 +0100

    #EDAC-34 API System - Get information about celestial bodies in a system / Tests

--- tests/edsm/test_systemApiBodies.py
+++ tests/edsm/test_systemApiBodies.py
@@ -1,28 +1,11 @@
 import unittest
 
-from connector.edsm.edsm_cntl import edsmCntl
+from src.connector.edsm.systemApi import bodies
 
 
-class EdsmApiTests(unittest.TestCase):
-
-    # Api System Station
-    def test_getStation(self):
-        json = edsmCntl.getStation("Gacrux")
-        # System
-        assert json['id'] == 7564
-        assert json['id64'] == 672028239289
-        assert json['name'] == 'Gacrux'
-        # TODO: Stations checken
-
-    def test_get_station_by_id(self):
-        json = edsmCntl.getStationById(9324)
-        assert json['id'] == 9324
-        assert json['id64'] == 2871051494841
-        assert json['name'] == 'Honoto'
-
-    # Api System Bodies
+class test_systemApiBodies(unittest.TestCase):
     def test_get_bodies(self):
-        json = edsmCntl.getBodies("HD 43193")
+        json = bodies.getBodies("HD 43193")
         # system data
         self.assertEqual(85920, json['id'])
         self.assertEqual("HD 43193", json['name'])
@@ -55,8 +38,7 @@ class EdsmApiTests(unittest.TestCase):
         self.assertFalse(planet['rotationalPeriodTidallyLocked'])
 
     def test_get_bodies_by_id(self):
-        json = edsmCntl.getBodiesById(85920)
-
+        json = bodies.getBodiesById(85920)
         # system data
         self.assertEqual(85920, json['id'])
         self.assertEqual("HD 43193", json['name'])
